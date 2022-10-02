2516b4556a0a7f621363cdbe40fe2e1f02240d55
robester0403 <robkso@gmail.com>
2022-10-02 10:02:24 -0400

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Techstack from "./pages/Techstack";
 
-function App() {
+const App: React.FC = () => {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const portfolioRef = useRef<HTMLDivElement>(null);
+  const techstackRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home ref={homeRef} />
+              <About ref={aboutRef} />
+              <Techstack ref={techstackRef} />
+              <Portfolio ref={portfolioRef} />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = React.forwardRef<HTMLDivElement>((ref: any) => {
+  return <div>About</div>;
+});
+
+export default About;
